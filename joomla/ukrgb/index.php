b4f0efbe3d8ae8a1256d7a08e868db517d9662ec
mrfg <markg@wler.co.uk>
2015-08-25 06:57:49 +0100

    Add Google analicts

--- joomla/ukrgb/index.php
+++ joomla/ukrgb/index.php
@@ -78,6 +78,22 @@ if ($this->countModules( 'aside' ) == 0){
 <jdoc:include type="head" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
+<!-- Google --> 
+<script>
+    
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  
+  ga('create', 'UA-26908210-1', 'auto');
+  ga('require', 'displayfeatures');
+  ga('set', '&uid', '<?php echo $user->id; ?>'); 
+  ga('send', 'pageview');
+
+</script>
+
 </head>
 
 <body>
