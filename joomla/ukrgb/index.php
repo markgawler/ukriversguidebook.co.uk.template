ce8f98900aba08968a90b7a39eaf1cb381968933
mrfg <markg@wler.co.uk>
2015-12-20 13:51:21 +0000

    Restore Google Analytics and Skim links code. remove second JQuery library.

--- joomla/ukrgb/index.php
+++ joomla/ukrgb/index.php
@@ -200,6 +200,22 @@ if ($this->countModules( 'aside' ) == 0){
 	</footer>
 	<jdoc:include type="modules" name="debug" style="none" />
 
+<!-- Google --> 
+<script>
+  if (window != top) top.location.href = location.href;
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  
+  ga('create', 'UA-26908210-1', 'auto');
+  ga('require', 'displayfeatures');
+  ga('set', '&uid', '<?php echo $user->id; ?>'); 
+  ga('send', 'pageview');
+
+</script>	
+<script type="text/javascript" src="//s.skimresources.com/js/71630X1520410.skimlinks.js"></script>
 
 </body>
 </html>
